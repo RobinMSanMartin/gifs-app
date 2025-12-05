9953cefce2c0c32f1918bc067622f40e56fceed6
RobinMSanMartin <robin.m.sanmartin@gmail.com>
2025-12-04 22:45:22 -0300

    added mapper file

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,23 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import type { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
 
-  constructor(){
+  trendingGifs = signal<Gif[]>([]);
+
+  constructor() {
     this.loadTrendingGifs();
   }
 
-  loadTrendingGifs(){
-    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`,{
-      params:{
-        api_key: environment.giphyApiKey,
-        limit: 20,
-      }
-    });
-  }
+  loadTrendingGifs() {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
 
+        console.log('gifs', { gifs });
+      });
+  }
 }
